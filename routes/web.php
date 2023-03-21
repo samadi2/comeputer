362b029a9117576e3f804c79574c2a2a27c82a46
samadi2 <satourdine@gmail.com>
2023-03-21 09:39:47 +0100

    ajout

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Admin\ArticleController as AdminArticleController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\CartController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,14 +17,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/articles', [ArticleController::class, 'index'])
     ->name('articles.index');
 Route::get('/articles/{article}', [ArticleController::class, 'show'])
     ->name('articles.show');
 // Les routes de gestion du panier
-Route::get('/cart', [CartController::class, 'show'])->name('cart.show');
+Route::get('cart', [CartController::class, 'show'])->name('cart.show');
+Route::post('cart/add/{article}',[CartController::class, 'add'])->name('cart.add');
+Route::get('cart/remove/{article}', [CartController::class, 'remove'])->name('cart.remove');
+Route::get('cart/empty', [CartController::class, 'empty'])->name('cart.empty');
 
 
 
